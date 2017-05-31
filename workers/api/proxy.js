489c563967a098658fc97c85071c3d6f0a78be98
face <face@myutil.com>
2017-05-30 22:01:19 -1000

    lint

--- workers/api/proxy.js
+++ workers/api/proxy.js
@@ -1,10 +1,8 @@
 'use strict'
 
-const _ = require('lodash')
 const Base = require('./base')
 
 class Proxy extends Base {
-
   space (service, msg) {
     const space = super.space(service, msg)
     return space
